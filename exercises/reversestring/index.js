861dbe8c5d662e129dcf05b3bc475aa4e14f665e
Michael J. Lyons <mikelyons7@me.com>
2018-11-30 16:31:39 -0700

    Complete reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,9 +7,22 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  const letters = str.split('');
-  const rev = letters.reduce((word, char) => char + word, '');
-  return rev;
+  return str.split('').reduce((rev, char) => char + rev, '');
 }
+
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+
+// function reverse(str) {
+//   let reversed = '';
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
 
 module.exports = reverse;
