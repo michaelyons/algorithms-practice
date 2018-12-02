94046b42e55c9977d0ab3ab7d78a5210e59370d3
Michael J. Lyons <mikelyons7@me.com>
2018-12-01 19:37:15 -0700

    Complete palindrome2

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,12 +7,18 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
+// function palindrome(str) {
+//   const rev = str
+//     .split('')
+//     .reverse()
+//     .join('');
+//   return rev === str;
+// }
+
 function palindrome(str) {
-  const rev = str
-    .split('')
-    .reverse()
-    .join('');
-  return rev === str;
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
 }
 
 module.exports = palindrome;
